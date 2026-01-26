143f06e1ff7f1433bb7a45f0410dadee7a4c222b
Sam <104291686+VoidSoup42@users.noreply.github.com>
2026-01-26 13:40:50 +0530

    Added precompiled headers and linked glfw

---
+++ Acroy/source/AcroyPCH.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+// STL
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <functional>
--- Acroy/source/Core/Log.cpp
+++ Acroy/source/Core/Log.cpp
@@ -1,3 +1,4 @@
+#include "AcroyPCH.hpp"
 #include "Log.hpp"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
